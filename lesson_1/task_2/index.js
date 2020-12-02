63fff645fea84dee5d195aab88a0cc56dda7a891
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-02 18:52:21 +0300

    l2 commit

--- lesson_1/task_2/index.js
+++ lesson_1/task_2/index.js
@@ -22,7 +22,7 @@ const rootElement = document.querySelector('#root');
 const elem = React.createElement(
     'div', { className: 'greeting' },
     React.createElement(
-        'h1', { className: 'greeting_title' }, 'Hello, world'
+        'h1', { className: 'greeting_title' }, 'Hello, world!'
     ),
     React.createElement(
         'p', { className: 'greeting__text' }, 'I\'m learning React'
