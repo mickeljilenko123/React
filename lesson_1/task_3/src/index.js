8caceb4ba40328030533c6a7ba9173d094aebcf4
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-03 15:46:21 +0300

    l2 commit

--- lesson_1/task_3/src/index.js
+++ lesson_1/task_3/src/index.js
@@ -7,9 +7,12 @@ const rootElement = document.querySelector('#root');
 const element = ( <
     div className = "greeting" >
     <
-    div className = "greeting__title" > Hello, world! < /div> <
+    div className = "greeting__title" > Hello, world! < /div>  <
     div className = "greeting__text" > I 'm learning React</div> <
     /div>
+
+
+
 );
 
 console.log(element)
