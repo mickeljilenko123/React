66f7508a18c11195f93c2688e1555bef2cc4a0f8
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-04 19:29:35 +0300

    l1 commit

---
+++ lesson_1/todo-list/src/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const rootElement = document.querySelector('#root');
+
+const element = ( <
+    h1 class = "title" > Todo List < /h1>
+);
+
+ReactDOM.render(element, rootElement);
