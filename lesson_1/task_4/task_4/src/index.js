e7c8b5755cb5ccb9cf3c0da4d5f57c7045784b11
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-04 18:40:13 +0300

    t2 commit

---
+++ lesson_1/task_4/task_4/src/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.querySelector('#root');
+
+const element = ( <
+    div className = "greeting" >
+    <
+    div className = "greeting__title" > Hello, world! < /div> <
+    div className = "greeting__text" > I 'm learning React</div> <
+    /div>
+);
+
+ReactDOM.render(element, rootElement);
