08c45c9b7e49eaabea81177240f82684d6dc3063
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-08 16:06:52 +0300

    l3 commit

--- lesson_3/task_3/src/App.jsx
+++ lesson_3/task_3/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Comment from "./Comment";
 
-const userInfo = {
+const user = {
   name: "Mikhailo",
   avatarUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
 };
@@ -9,7 +9,7 @@ const userInfo = {
 const App = () => {
   return (
     <Comment
-      user={userInfo}
+      author={user}
       text="Good job!"
       date={new Date("2020-01-01T05:01:01.566Z")}
     />
