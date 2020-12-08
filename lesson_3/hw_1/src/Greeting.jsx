2faa3820bdede6ab48318245822478c2ac701cb4
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-08 19:04:52 +0300

    l3 commit

--- lesson_3/hw_1/src/Greeting.jsx
+++ lesson_3/hw_1/src/Greeting.jsx
@@ -3,9 +3,10 @@ import "./index.scss";
 import moment from "moment";
 
 const Greeting = props => {
+   const age = Math.floor((new Date() - new Date(props.birthDate)) / 1000 / 60 / 60 / 24 / 365.25);
    return (
     <div className="greeting">
-    {`My name is ${props.firstName} ${props.lastName} . I'm ${props.birthDate} years old`}
+    {`My name is ${props.firstName} ${props.lastName} . I'm ${age} years old`}
     </div>
    )
 }
--- lesson_3/hw_1/src/index.jsx
+++ lesson_3/hw_1/src/index.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from "./Greeting.jsx";
 import moment from "moment";
+import App from "./App.jsx";
+
 
 const rootElement = document.querySelector('#root');
 
 
 
 ReactDOM.render(
-<Greeting 
-firstName = "John"
-lastName = "Doe"
-birthDate = {new Date("1995-01-01T05:01:01.566Z")}
+<App
 />, 
 rootElement);
