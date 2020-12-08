6443fe30002c030e897b1c02d1b70c1ecf65ed52
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-08 19:17:27 +0300

    l3 commit

--- lesson_3/hw_1/src/Greeting.jsx
+++ lesson_3/hw_1/src/Greeting.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import "./index.scss";
 
 
-const Greeting = props => {
-   const age = Math.floor((new Date() - new Date(props.birthDate)) / 1000 / 60 / 60 / 24 / 365.25);
-   return (
+const Greeting = (props) => {
+
+  const age = Math.floor((new Date() - new Date(props.birthDate)) / 1000 / 60 / 60 / 24 / 365.25);
+  return (
     <div className="greeting">
-    {`My name is ${props.firstName} ${props.lastName} . I'm ${age} years old`}
+      {`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}
     </div>
-   )
-}
-export default Greeting;
+  );
+};
+
+export default Greeting
