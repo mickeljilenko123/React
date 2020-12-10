069804243402d10af420531d236b211d29c266cf
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-10 21:16:59 +0300

    r3 commit

--- lesson_4/task_1/src/App.jsx
+++ lesson_4/task_1/src/App.jsx
@@ -3,7 +3,11 @@ import Counter from './Counter.jsx';
 
 const App = () => {
     return (
-      <Counter />
+        <>
+      <Counter start= {5}/>
+      <Counter start= {12}/>
+      <Counter start= {-21}/>
+      </>
     )
 };
 
--- lesson_4/task_1/src/Counter.jsx
+++ lesson_4/task_1/src/Counter.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import './counter.scss';
 
 class Counter extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            counter: 0
+            counter: props.start
         };
         setInterval(() => {
             this.setState({
                 counter: this.state.counter + 1
             });
-        }, 1000)
+        }, props.interval)
     }
     render() {
         return (
