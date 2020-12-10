f4919e3afdfaf9839d5d270b7af6d1e7298ebe16
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-10 21:53:00 +0300

    r4 commit

--- lesson_4/task_1/src/Counter.jsx
+++ lesson_4/task_1/src/Counter.jsx
@@ -6,7 +6,7 @@ class Counter extends Component {
         super(props);
         this.state = {
             counter: props.start
-        };
+        }
         setInterval(() => {
             this.setState({
                 counter: this.state.counter + 1
@@ -16,7 +16,7 @@ class Counter extends Component {
     render() {
         return (
             <div className="counter">{this.state.counter}</div>  
-        )
+        );
     }
 }
 
