ff6b4d572fe161e9ddb9dff4e25f326fd920672b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-12 22:47:06 +0300

    r5 commit

---
+++ lesson_5/task_4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Color from './Color';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Color />, rootElement);
