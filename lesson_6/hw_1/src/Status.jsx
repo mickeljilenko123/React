1155cc9024f24a8e8976c47e57bfa839fdf3c8b9
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-15 15:46:18 +0300

    r6 commit

--- lesson_6/hw_1/src/Status.jsx
+++ lesson_6/hw_1/src/Status.jsx
@@ -3,7 +3,7 @@ import Online from './Online';
 import Offline from './Offline';
 
 const Status = ({ isOnline }) => 
-isOnline ? (< Online />) : (< Offline />);
+isOnline ? (<Online />) : (<Offline />);
 
 export default Status;
    
