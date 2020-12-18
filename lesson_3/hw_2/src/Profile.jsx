97c9e68245caa04fbc294d6459e4028a85a0dd90
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-18 18:38:30 +0300

    r8 commit

--- lesson_3/hw_2/src/Profile.jsx
+++ lesson_3/hw_2/src/Profile.jsx
@@ -3,16 +3,18 @@ import moment from "moment";
 
 
 const Profile = (props) => {
-  const date = moment(props.birthDate).format('DD MMM YY');
 
-  return (
-    <div className="profile">
-      <div className="profile__name">
-        <span>{`${props.userData.firstName} ${props.userData.lastName}`}</span>
-      </div>
-      <span className="profile__birth">{`Was born ${date} in ${props.userData.birthPlace}`}</span>
+  const date = moment(props.userData.birthDate).format("DD MMM YY");
+  return(
+    <>
+    <div className = "profile__name">
+      <span>{props.userData.firstName} {props.userData.lastName}</span>
     </div>
-  );
+    <div className = "profile__birth">
+    <span>{` Was born ${date} in ${props.userData.birthPlace}`}</span>
+    </div>
+    </>
+  )
 };
 
 export default Profile
