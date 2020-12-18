18547ca1beb9e74b3ceaf253c6ce4618cf6764e3
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-18 17:31:11 +0300

    r8 commit

--- lesson_3/hw_1/src/Greeting.jsx
+++ lesson_3/hw_1/src/Greeting.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 
 
 const Greeting = props => {
-    const years = Math.floor(new Date(props.user.birthDate) / (1000 * 60 * 60 * 24 * 365.25));
+    const years = Math.floor((new Date() - new Date(props.user.birthDate)) / (1000 * 60 * 60 * 24 * 365.25));
     return (
         <div className="greeting">
           {`My name is ${props.user.firstName} ${props.user.lastName}. I'm ${years} years old`}
