b8617907d86c7a944ed7dd4f1a8991633df0f1c2
mickeljilenko123 <mickeljilenko@gmail.com>
2020-12-31 18:45:35 +0300

    r10 commit

---
+++ lesson_10/task_3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Calculator from "./Calculator";
+
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Calculator />, rootElement);
