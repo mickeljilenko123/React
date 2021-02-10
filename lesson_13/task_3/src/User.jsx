c9e2e7fd8fe876d8052adc3e909433ee2c961df6
mickeljilenko123 <mickeljilenko@gmail.com>
2021-02-10 16:08:37 +0300

    init

--- lesson_13/task_3/src/User.jsx
+++ lesson_13/task_3/src/User.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 
 class User extends React.Component {
+
+  state = {
+     avatar: null,
+     name: null,
+     location: null,
+  }
+  componentDidMount() {
+    this.getUserData(this.props.match.params.userId);
+  }
+  componentDidUpdate(prevProps) {
+    const curUserId = this.props.match.params.userId;
+    if(prevProps.match.params.userId != curUserId) {
+      this.getUserData(curUserId)
+    }
+  }
+
+  getUserData = userId => {
+     fetch(`https://api.github.com/users/${userId}`)
+     .then(response => response.json())
+     .then(userData => {
+       const {avatar_url, name, location } = userData;
+       this.setState({
+         avatar: null,
+         name: null,
+         location: null,
+       })
+     })
+  }
+
   render() {
+    const { avatar, name, location } = this.state
+    if (!avatar || !name || !location) {
+      return null;
+    }
     return (
       <div class="user">
         <img alt="User Avatar" 
-           src="https://avatars1.githubusercontent.com/u/9919?v=4" 
+           src={avatar} 
            class="user__avatar"
         />
         <div class="user__info">
-          <span class="user__name">GitHub</span>
-          <span class="user__location">San Francisco,CA</span>
+          <span class="user__name">{name}</span>
+          <span class="user__location">{location}</span>
         </div>
       </div>
     );
--- lesson_13/task_3/src/Users.jsx
+++ lesson_13/task_3/src/Users.jsx
@@ -3,30 +3,27 @@ import User from "./User";
 import { Link, Route, Switch } from 'react-router-dom';
 
 
-class Users extends React.Component {
+const Users = ({ match }) => {
 
-  render() {
     return (
       <div className="page__content">
         <h1>Users</h1>
         <ul className="navigation">
           <li className="navigation__item">
-            <Link to="/users/github">Github</Link>
+            <Link to={`${match.path}/github`}>Github</Link>
           </li>
           <li className="navigation__item">
-            <Link to="/users/facebook">Facebook</Link>
+            <Link to={`${match.path}/facebook`}>Facebook</Link>
           </li>
         </ul>
         <Switch>
-          <Route exact path="/users" >
+          <Route exact path={match.path} >
             <span>Select a user please</span>
           </Route>
-          <Route exact path="/users/:userId" component={User}></Route>
+          <Route exact path={`${match.path}/:userId`} component={User}></Route>
         </Switch>
       </div>
     )
   }
-
-}
 
 export default Users;
